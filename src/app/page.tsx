00673c5d39ebcde114b5ce247d000eacfc33baaa
HuynhKhanh1402 <khanh@duck.com>
2026-05-01 17:59:41 +0700

    feat: render home page with prerendered shell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,58 @@
-export default function Home() {
-  return <main className="p-8">Home placeholder</main>
+import type { Metadata } from 'next'
+import Footer from '@/features/home/Footer'
+import GameModes from '@/features/home/GameModes'
+import Hero from '@/features/home/Hero'
+import JoinGuide from '@/features/home/JoinGuide'
+import Navbar from '@/features/home/Navbar'
+
+export const unstable_instant = { prefetch: 'static', unstable_disableBuildValidation: true }
+
+export const metadata: Metadata = {
+  title: 'Máy chủ Minecraft #1 Việt Nam',
+}
+
+const homeJsonLd = {
+  '@context': 'https://schema.org',
+  '@graph': [
+    {
+      '@type': 'Organization',
+      '@id': 'https://kingmc.vn#org',
+      name: 'KingMC',
+      url: 'https://kingmc.vn',
+      logo: 'https://kingmc.vn/og/default.png',
+      sameAs: [
+        'https://discord.gg/kingmcvn',
+        'https://www.facebook.com/kingmcvn/',
+        'https://www.youtube.com/@kingmc_vietnam',
+      ],
+    },
+    {
+      '@type': 'WebSite',
+      '@id': 'https://kingmc.vn#site',
+      url: 'https://kingmc.vn',
+      name: 'KingMC',
+      description: 'Máy chủ Minecraft #1 Việt Nam',
+      inLanguage: 'vi',
+    },
+  ],
+}
+
+export default function HomePage() {
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(homeJsonLd) }}
+      />
+      <div className="relative min-h-screen overflow-x-clip">
+        <Navbar />
+        <main>
+          <Hero />
+          <GameModes />
+          <JoinGuide />
+        </main>
+        <Footer />
+      </div>
+    </>
+  )
 }
