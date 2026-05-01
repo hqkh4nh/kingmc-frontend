7f56576cfea581931d7d3b86fd75d6b4bf66543e
HuynhKhanh1402 <khanh@duck.com>
2026-05-01 17:25:13 +0700

    chore: configure next core and typescript paths

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import bundleAnalyzer from '@next/bundle-analyzer'
+
+const withBundleAnalyzer = bundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'standalone',
+  cacheComponents: true,
+  experimental: {
+    instantNavigationDevToolsToggle: true,
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+  poweredByHeader: false,
+  reactStrictMode: true,
+}
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig)
